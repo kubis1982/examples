52082cfbaa31b6c3c94c325a384aa0fb2f7a3302
m.swiatnicki <mariusz.swiatnicki@gmail.com>
2025-02-22 19:42:16 +0100

    Dodano react-router-dom do MUI

--- REACT/MaterialUI/src/main.tsx
+++ REACT/MaterialUI/src/main.tsx
@@ -2,9 +2,34 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import HomePage from './pages/home.tsx'
+import OrdersPage from './pages/orders.tsx'
+
+const router = createBrowserRouter([
+  {
+    Component: App,
+    children: [
+      {
+        path: '/',
+        Component: HomePage,
+        children: [
+          {
+            path: '',
+            Component: HomePage,
+          },
+          {
+            path: 'orders',
+            Component: OrdersPage,
+          },
+        ],
+      }
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
