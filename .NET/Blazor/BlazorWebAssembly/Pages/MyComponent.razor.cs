b6a929f100e49d068b1e2431ad18534ba5169c94
kubis1982 <mariusz.swiatnicki@gmail.com>
2025-04-06 07:48:49 +0200

    Dodano Blazor.Bootstrap do projektu Bootstrap

---
+++ .NET/Blazor/BlazorWebAssembly/Pages/MyComponent.razor.cs
@@ -0,0 +1,32 @@
+﻿namespace BlazorWebAssembly.Pages
+{
+    using System.Threading.Tasks;
+
+    public partial class MyComponent
+    {
+        private int currentValue;
+        private string? message;
+
+        private void Increment()
+        {
+            message = string.Empty;
+            currentValue++;
+        }
+
+        private void Decrement()
+        {
+            if (currentValue == 0) {
+                message = "Próba ustawienia wartości poniżej zera";
+                return;
+            };            
+            currentValue--;
+        }
+
+        protected override Task OnInitializedAsync()
+        {
+            currentValue += 10;
+
+            return base.OnInitializedAsync();
+        }
+    }
+}
